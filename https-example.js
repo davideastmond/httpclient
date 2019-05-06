a67441d2cf61242fe61b679bc91ee4888120cdbb
David Eastmond <davebuck1981@gmail.com>
2019-05-05 23:10:50 -0400

    added http request

--- https-example.js
+++ https-example.js
@@ -1,3 +1,18 @@
 var https = require("https");
 
-console.log("I did it!");
+var params = {
+  host: "www.example.com",
+  path: "/"
+};
+
+// Call back function
+
+function callback() {
+  console.log("In response handler callback!");
+}
+
+console.log("About to make request...");
+
+https.request(params, callback).end();
+
+console.log("I've made the request");
